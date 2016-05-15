2a1dbd741f7c4c06eedd0aacf06a9cd51aa92e84
Robert Prine <robertprine2@yahoo.com>
2016-05-15 18:55:24 -0400

    Finally got the object in an object on firebase working thanks to Caleb

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -22,7 +22,7 @@ $(document).ready(function(){
 			{name: "lizard",
 			image: "<img src='assets/images/lizard.png'>"},
 			{name: "spork",
-			image: "<img src='assets/images/spork.png'>"},
+			image: "<img src='assets/images/spork.png'>"}
 			],
 
 		// variables used for player objects
@@ -37,7 +37,23 @@ $(document).ready(function(){
 
 		ties: 0,
 
-		player: 1,
+		name2: "",
+
+		pick2: "",
+
+		wins2: 0,
+
+		losses2: 0,
+
+		ties2: 0,
+
+		turn: 1,
+
+		players: 'players',
+
+		player1: 1,
+
+		player2: 2,
 
 		// When name is entered create an object in firebase with name losses wins and ties...in addition create text instead of login box that says, "Hi obj.name! You are player 1. (It's your turn![only after player 2 has joined])" or "Hi obj.name! You are player 2. You are waiting for player 1 to make their choice."....also prints obj.name, choices (r,p,s,l,sp[only after player 2 has logged in as well]), and wins, losses and ties in that order...Also highlights by changin the border color of the player whos turn it is. Once both have joined make turn counter in firebase.
 
@@ -45,30 +61,50 @@ $(document).ready(function(){
 
 			$("#connect").on('click', function() {
 
+				// assigns user input as game.name
+
 				game.name = $("#name-input").val().trim();
 
-				console.log(game.name);
+				game.dataInfo.once("value", function(snapshot) {
 
-				// pushes data to firebase
+					var exists = snapshot.child(game.players).exists();
+					var full = snapshot.child(game.players).child(game.player2).exists();
+					game.assignPlayer(game.name, game.dataInfo, exists, full);
 
-				game.dataInfo.push({
-					name: game.name,
-					pick: game.pick,
-					wins: game.wins,
-					losses: game.losses,
-					ties: game.ties,
-					player: game.player
-				});
+					// *******replaces name input with 
 
-				// if player is 1 then add 1, else subtract 1
+					$("#nameForm").html("Hi " + game.name + "! You are player " + game.player + ".");
+				}); // end of game.dataInfo.once
 
-				if (game.player == 1) {
-					game.player++;
-				}
+// **************	// pushes data to firebase
 
-				else {
-					game.player--;
-				}
+// 				game.dataInfo.push({
+// 						name: game.name,
+// 						pick: game.pick,
+// 						wins: game.wins,
+// 						losses: game.losses,
+// 						ties: game.ties,
+// 						player: game.click
+// 				});
+
+// 				// ********Changes what is in player1 or player2 box
+
+// 					$("#player1").html("<h3>" + game.name + "</h3");
+
+// 					for (var i = 0; i < game.pieces.length; i++) {
+
+// 						var p = $('<button>');
+// 						p.addClass("piece");
+// 						p.attr('data-index', i);
+// 						p.attr('data-name', game.pieces[i].name);
+// 						p.append('<p>' + game.pieces[i].name + '</p>');
+// 						p.append(game.pieces[i].image);
+
+// 						$("#player1").append(p);
+
+// 					} // ends for loop
+
+// 					$("#player1").append("<p>Wins: "+ game.wins + " Losses: " + game.losses + " Ties: " + game.ties + "</p>");
 
 				// Doesn't refresh the forms
 
@@ -78,28 +114,61 @@ $(document).ready(function(){
 
 		}, // end of connect function
 
-		// does this function whenever a value in firebase changes
+		// does this function to assign players their own objects or tell them that the game is full
 
-		fireData: function() {
-			game.dataInfo.on('value', function(snapshot) {
+		assignPlayer: function(name, dataInfo, exists, full) {
 
-				console.log(snapshot.val());
+			var playersRef = game.dataInfo.child(game.players);
 
-			}, function (errorObject) {
-				console.log("The read failed: " + errorObject.code);
-			})
-		},
+			if (exists && !full) {
+				
+				var player2Ref = playersRef.child(game.player2);
+				player2Ref.set({
+					name: game.name,
+					pick: game.pick,
+					wins: game.wins,
+					losses: game.losses,
+					ties: game.ties
+				});
+			} // end if exists and not full
+
+			else if(full) {
+				
+				alert('The game is full, try again later.');
+			
+			} // end of else if full
+
+			else {
+
+				var player1Ref = playersRef.child(game.player1);
+
+				player1Ref.set({
+					name: game.name,
+					pick: game.pick,
+					wins: game.wins,
+					losses: game.losses,
+					ties: game.ties
+				});
+
+			} // end of else player1
+
+		}, // end of assignPlayer function
+
+		// logic for who wins in the game
+
+		// logic: function() {
+
+		// 	if (game.pick)
+
+		// }, // end of logic function
 
 	} // Ends game object
+
+	
 
 	// When name is entered create an object in firebase with name losses wins and ties...in addition create text instead of login box that says, "Hi obj.name! You are player 1. (It's your turn![only after player 2 has joined])" or "Hi obj.name! You are player 2. You are waiting for player 1 to make their choice."....also prints obj.name, choices (r,p,s,l,sp[only after player 2 has logged in as well]), and wins, losses and ties in that order...Also highlights by changin the border color of the player whos turn it is. Once both have joined make turn counter in firebase.
 
 	game.connect();
-	
-	game.fireData();
-
-
-	// When name is entered create an object in firebase with name losses wins and ties...in addition create text instead of login box that says, "Hi obj.name! You are player 1. (It's your turn![only after player 2 has joined])" or "Hi obj.name! You are player 2. You are waiting for player 1 to make their choice."....also prints obj.name, choices (r,p,s,l,sp[only after player 2 has logged in as well]), and wins, losses and ties in that order...Also highlights by changin the border color of the player whos turn it is. Once both have joined make turn counter in firebase.
 
 	// Player 1 picks their choice showing them what they chose, but not player 2. Their choice is added to firebase. The turn counter goes up highlighting the other players play area.
 
